73f9424b75dd1c9a5e3c4449bb9d3fd9c39b4d43
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-05-14 21:12:49 +0100

    Update RaycastEdited.lua

--- OneHanded/Raycast/RaycastEdited.lua
+++ OneHanded/Raycast/RaycastEdited.lua
@@ -111,7 +111,7 @@ script.Parent.MainScript.Function.OnServerEvent:connect(function(Player, Action,
 			local dmg = math.random(mindmg,maxdmg)
 			local gui = script.DMGGui:clone()
 			coroutine.wrap(function()
-				repeat wait() until gui.Parent.Name == "Head"
+				repeat wait() until gui and gui.Parent and gui.Parent.Name == "Head"
 
 				local dmg = tonumber(script.Parent.Dmg.Text)
 				local gui = gui
